5b0d03aa47071c5d89d727ebea29bd3bae02704d
Maciej Fedorowiat <maciej.fedorowiat@raisin.com>
2022-06-13 17:28:43 +0200

    First Exercise

--- src/service/verb.ts
+++ src/service/verb.ts
@@ -1,5 +1,55 @@
 import { readAll } from '../repository/repository';
+import { RegularVerb } from '../repository/db';
 
-export const getRandomVerb = () => {
-    return readAll().verbs.regular[0];
+export enum Person {
+    Eu = 'Eu',
+    Tu = 'Tu',
+    ElaEleVocé = 'Ela/Ele/Vocé',
+    Nós = 'Nós',
+    ElesElasVosēs = 'Eles/Elas/Vosēs'
+}
+
+export const getRandomVerb: () => string = () => {
+    return getRandomElement(readAll().verbs.regular);
+}
+
+export const getCorrectConjugation: (verb: RegularVerb, person: Person) => string = (verb, person) => {
+    if (verb.endsWith('ar') || verb.endsWith('er')) {
+        switch (person) {
+            case 'Eu':
+                return verb.substring(0, verb.length - 2) + 'o'
+            case 'Tu':
+                return verb.substring(0, verb.length - 2) + 'es'
+            case 'Ela/Ele/Vocé':
+                return verb.substring(0, verb.length - 1);
+            case 'Nós':
+                return verb.substring(0, verb.length - 1) + 'mos'
+            case 'Eles/Elas/Vosēs':
+                return verb.substring(0, verb.length - 1) + 'm'
+        }
+    }
+    if (verb.endsWith('ir')) {
+        switch (person) {
+            case 'Eu':
+                return verb.substring(0, verb.length - 2) + 'o'
+            case 'Tu':
+                return verb.substring(0, verb.length - 2) + 'es'
+            case 'Ela/Ele/Vocé':
+                return verb.substring(0, verb.length - 2) + 'e';
+            case 'Nós':
+                return verb.substring(0, verb.length - 1) + 'mos'
+            case 'Eles/Elas/Vosēs':
+                return verb.substring(0, verb.length - 2) + 'em'
+        }
+    }
+    throw Error('Incorrect Regular Verb');
+}
+
+
+export const getRandomPerson: () => Person = () => {
+    return getRandomElement(Object.values(Person))
+}
+
+function getRandomElement<T> (arr: T[]): T {
+    return arr[Math.floor(Math.random() * arr.length)]
 }
