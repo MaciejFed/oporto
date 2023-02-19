231c7183d50041be65147c79f1815faae1937af0
MaciejFed <MFedorowiat@gmail.com>
2023-02-19 13:11:06 +0100

    Words Without Examples (#49)

---
+++ src/service/conjugation.ts
@@ -0,0 +1,32 @@
+import { readAll, Verb } from '../repository/exercises-repository';
+import { VerbExerciseGenerator } from '../exercise/generator';
+import { VerbExercise } from '../exercise/verb-exercise';
+
+const getConjugationsForVerb = (verb: Verb) => {
+  // eslint-disable-next-line new-cap
+  return VerbExerciseGenerator()
+    .filter((verbExercise) => (verbExercise as VerbExercise).verb.infinitive === verb.infinitive)
+    .map((verbExercise) => verbExercise.getCorrectAnswer())
+    .concat(verb.infinitive)
+    .sort((a, b) => a.localeCompare(b));
+};
+
+export const getAllWordsConjugations = () => {
+  const allVerbsForms = readAll().verbs.flatMap((verb) => getConjugationsForVerb(verb));
+  const allNounForms = readAll().nouns.flatMap((noun) => [noun.portuguese.word, noun.portuguese.plural || '']);
+  const allAdjectiveForms = readAll().adjectives.flatMap((adjective) => [
+    adjective.feminine.singular,
+    adjective.feminine.plural,
+    adjective.masculine.singular,
+    adjective.masculine.plural
+  ]);
+  const others = readAll().others.map((other) => other.portuguese);
+
+  return allVerbsForms
+    .concat(allNounForms)
+    .concat(allAdjectiveForms)
+    .concat(others)
+    .filter((word) => word)
+    .map((word) => word.toLowerCase())
+    .sort();
+};
