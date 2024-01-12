8de8f40c282cd5ea00a08abd4bb1714f64a935d1
MaciejFedorowiat <maciejfedorowiat@gmail.com>
2024-01-12 17:25:03 +0100

    Fix Tests

--- src/server/server.ts
+++ src/server/server.ts
@@ -86,6 +86,5 @@ app.post('/example/find', async (req: Request, res: Response) => {
 
 app.listen(port, async () => {
   logger.info('Starting server...');
-  await preFetch();
   logger.info(`[server]: Server is running at http://localhost:${port}`);
 });
