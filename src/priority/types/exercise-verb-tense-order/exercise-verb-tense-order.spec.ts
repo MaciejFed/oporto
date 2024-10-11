ce802b2437e3c8b42e2751fbd97baedfb25c093e
MaciejFedorowiat <maciejfedorowiat@gmail.com>
2024-10-11 22:02:15 +0200

    terminal

--- src/priority/types/exercise-verb-tense-order/exercise-verb-tense-order.spec.ts
+++ src/priority/types/exercise-verb-tense-order/exercise-verb-tense-order.spec.ts
@@ -1,8 +1,5 @@
 import { VerbExercise } from '../../../exercise/verb-exercise';
-import {
-  EXERCISE_VERB_TENSE_ORDER,
-  exerciseVerbTenseOrder,
-} from './exercise-verb-tense-order';
+import { EXERCISE_VERB_TENSE_ORDER, exerciseVerbTenseOrder } from './exercise-verb-tense-order';
 import { VerbTranslationExercise } from '../../../exercise/translation/verb-translation-exercise';
 import { NounTranslationExercise } from '../../../exercise/translation/noun-translation-exercise';
 import { ExerciseResultContext, noPriority } from '../../priority';
@@ -14,92 +11,97 @@ const verb = wordDatabase.verb('estar');
 const verb2 = wordDatabase.verb('ter');
 const persons = [Person.Eu, Person.Tu, Person.ElaEleVocê, Person.Nós, Person.ElesElasVosēs];
 
-const toResult = (exercise: VerbExercise, wasCorrect = true) => ({ exercise, wasCorrect })
+const toResult = (exercise: VerbExercise, wasCorrect = true) => ({ exercise, wasCorrect });
 
-const getAllResults = () => [
-  persons.map((person) => VerbExercise.new(verb, person, 'presentSimple')).map((exercise) => toResult(exercise)),
-  [toResult(VerbExercise.new(verb, Person.Eu, 'pastPerfect'), false),
-  toResult(VerbExercise.new(verb, Person.Tu, 'pastPerfect'), true)],
+const getAllResults = () =>
+  [
+    persons.map((person) => VerbExercise.new(verb, person, 'presentSimple')).map((exercise) => toResult(exercise)),
+    [
+      toResult(VerbExercise.new(verb, Person.Eu, 'pastPerfect'), false),
+      toResult(VerbExercise.new(verb, Person.Tu, 'pastPerfect'), true)
+    ],
 
-  persons.map((person) => VerbExercise.new(verb2, person, 'presentSimple')).map((exercise) => toResult(exercise)),
-  persons.map((person) => VerbExercise.new(verb2, person, 'pastPerfect')).map((exercise) => toResult(exercise)),
-
-].flatMap((r) => r) as any as Result[]
-
-
+    persons.map((person) => VerbExercise.new(verb2, person, 'presentSimple')).map((exercise) => toResult(exercise)),
+    persons.map((person) => VerbExercise.new(verb2, person, 'pastPerfect')).map((exercise) => toResult(exercise))
+  ].flatMap((r) => r) as any as Result[];
 
 describe('exerciseVerbTenseOrder', () => {
-  const verbNeverDone = wordDatabase.verb('ser')
+  const verbNeverDone = wordDatabase.verb('ser');
   describe('None tense done', () => {
     it('present simple no bonus priority', () => {
       const verbExercise = VerbExercise.new(verbNeverDone, Person.Eu, 'presentSimple');
       const priority = exerciseVerbTenseOrder(verbExercise, {
-        exerciseSubjectResults: getAllResults(),
+        exerciseSubjectResults: getAllResults()
       } as ExerciseResultContext);
 
-      expect(priority).toEqual(noPriority(verbExercise))
+      expect(priority).toEqual(noPriority(verbExercise));
     });
 
     it('past perfect with bonus priority', () => {
       const verbExercise = VerbExercise.new(verbNeverDone, Person.Eu, 'pastPerfect');
       const priority = exerciseVerbTenseOrder(verbExercise, {
-        exerciseSubjectResults: getAllResults(),
+        exerciseSubjectResults: getAllResults()
       } as ExerciseResultContext);
 
-      expect(priority).toEqual([{
-        exercise: verbExercise,
-        priorityName: 'EXERCISE_VERB_TENSE_ORDER',
-        priorityValue: EXERCISE_VERB_TENSE_ORDER,
-      }])
+      expect(priority).toEqual([
+        {
+          exercise: verbExercise,
+          priorityName: 'EXERCISE_VERB_TENSE_ORDER',
+          priorityValue: EXERCISE_VERB_TENSE_ORDER
+        }
+      ]);
     });
 
     it('imperfect with bonus priority', () => {
       const verbExercise = VerbExercise.new(verbNeverDone, Person.Eu, 'imperfect');
       const priority = exerciseVerbTenseOrder(verbExercise, {
-        exerciseSubjectResults: getAllResults(),
+        exerciseSubjectResults: getAllResults()
       } as ExerciseResultContext);
 
-      expect(priority).toEqual([{
-        exercise: verbExercise,
-        priorityName: 'EXERCISE_VERB_TENSE_ORDER',
-        priorityValue: EXERCISE_VERB_TENSE_ORDER,
-      }])
-    })
-  })
+      expect(priority).toEqual([
+        {
+          exercise: verbExercise,
+          priorityName: 'EXERCISE_VERB_TENSE_ORDER',
+          priorityValue: EXERCISE_VERB_TENSE_ORDER
+        }
+      ]);
+    });
+  });
 
   describe('Present simple tense done', () => {
     it('past perfect with no bonus priority', () => {
       const verbExercise = VerbExercise.new(verb, Person.Eu, 'pastPerfect');
       const priority = exerciseVerbTenseOrder(verbExercise, {
-        exerciseSubjectResults: getAllResults(),
+        exerciseSubjectResults: getAllResults()
       } as ExerciseResultContext);
 
-      expect(priority).toEqual(noPriority(verbExercise))
+      expect(priority).toEqual(noPriority(verbExercise));
     });
 
     it('imperfect with bonus priority', () => {
       const verbExercise = VerbExercise.new(verb, Person.Eu, 'imperfect');
       const priority = exerciseVerbTenseOrder(verbExercise, {
-        exerciseSubjectResults: getAllResults(),
+        exerciseSubjectResults: getAllResults()
       } as ExerciseResultContext);
 
-      expect(priority).toEqual([{
-        exercise: verbExercise,
-        priorityName: 'EXERCISE_VERB_TENSE_ORDER',
-        priorityValue: EXERCISE_VERB_TENSE_ORDER,
-      }])
-    })
-  })
+      expect(priority).toEqual([
+        {
+          exercise: verbExercise,
+          priorityName: 'EXERCISE_VERB_TENSE_ORDER',
+          priorityValue: EXERCISE_VERB_TENSE_ORDER
+        }
+      ]);
+    });
+  });
 
   describe('Present simple and past perfect tense done', () => {
     it('imperfect no bonus priority', () => {
       const verbExercise = VerbExercise.new(verb2, Person.Eu, 'imperfect');
       const priority = exerciseVerbTenseOrder(verbExercise, {
-        exerciseSubjectResults: getAllResults(),
+        exerciseSubjectResults: getAllResults()
       } as ExerciseResultContext);
 
-      expect(priority).toEqual(noPriority(verbExercise))
+      expect(priority).toEqual(noPriority(verbExercise));
     });
-  })
-
+  });
 });
--- src/priority/types/exercise-verb-tense-order/exercise-verb-tense-order.ts
+++ src/priority/types/exercise-verb-tense-order/exercise-verb-tense-order.ts
@@ -10,36 +10,35 @@ export const EXERCISE_VERB_TENSE_ORDER = -1000;
 export const persons = [Person.Eu, Person.Tu, Person.ElaEleVocê, Person.Nós, Person.ElesElasVosēs];
 
 const isDoneForTime = (verb: Verb, results: Result[], verbTime: VerbTime) =>
-   persons.map((person) => VerbExercise.new(verb, person, verbTime))
+  persons
+    .map((person) => VerbExercise.new(verb, person, verbTime))
     .map((exercise) => getSingleExerciseProgress(results, exercise).progressType)
-     .filter((progress) => ProgressType.DONE === progress).length === 5;
-
+    .filter((progress) => ProgressType.DONE === progress).length === 5;
 
 const getTensesDone = (verb: Verb, results: Result[]) => ({
   presentSimple: isDoneForTime(verb, results, 'presentSimple'),
   pastPerfect: verb.pastPerfect?.Eu && isDoneForTime(verb, results, 'pastPerfect'),
-  imperfect: verb.imperfect?.Eu && isDoneForTime(verb, results, 'imperfect'),
-})
+  imperfect: verb.imperfect?.Eu && isDoneForTime(verb, results, 'imperfect')
+});
 
 export function exerciseVerbTenseOrder(
   exercise: Exercise,
   { exerciseSubjectResults }: ExerciseResultContext
 ): Priority[] {
-  if (
-    !(exercise instanceof VerbExercise)
-  ) {
+  if (!(exercise instanceof VerbExercise)) {
     return noPriority(exercise);
   }
   const verbExercise = exercise as VerbExercise;
   if (verbExercise.verbTime === 'presentSimple') return noPriority(exercise);
   const tensesDone = getTensesDone(verbExercise.verb, exerciseSubjectResults);
-  if (verbExercise.verbTime === 'pastPerfect' && tensesDone.presentSimple) return noPriority(exercise)
-  if (verbExercise.verbTime === 'imperfect' && tensesDone.pastPerfect) return noPriority(exercise)
+  if (verbExercise.verbTime === 'pastPerfect' && tensesDone.presentSimple) return noPriority(exercise);
+  if (verbExercise.verbTime === 'imperfect' && tensesDone.pastPerfect) return noPriority(exercise);
 
-
-  return [{
-    exercise,
-    priorityName: 'EXERCISE_VERB_TENSE_ORDER',
-    priorityValue: EXERCISE_VERB_TENSE_ORDER,
-  }];
+  return [
+    {
+      exercise,
+      priorityName: 'EXERCISE_VERB_TENSE_ORDER',
+      priorityValue: EXERCISE_VERB_TENSE_ORDER
+    }
+  ];
 }
