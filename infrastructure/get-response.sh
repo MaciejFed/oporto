81b3ff56453111c09118704a51592f8c2724c5f2
MaciejFedorowiat <maciejfedorowiat@gmail.com>
2025-07-05 15:26:57 +0200

    terminal

--- infrastructure/get-response.sh
+++ infrastructure/get-response.sh
@@ -3,8 +3,9 @@ RESPONSE=$(curl -H 'Authorization: Bearer 7B008FE3-B74D-4C70-B63B-AB2790E20D95' 
 
 header=$(echo "$RESPONSE" | jq -r '.header')
 body=$(echo "$RESPONSE" | jq -r '.body')
+example=$(echo "$RESPONSE" | jq -r '.example')
 
 # Use the variables in a POST request
 curl -X POST "https://api.pushover.net/1/messages.json" \
   -H "Content-Type: application/json" \
-  -d "{\"token\": \"az9487tia8hd2gwc7f4s3z6q9d3czc\", \"user\": \"upog9zsfzmzog2rkaguko9ri669taq\", \"message\": \"- $body\", \"title\": \"$header\"}"
+  -d "{\"token\": \"az9487tia8hd2gwc7f4s3z6q9d3czc\", \"user\": \"upog9zsfzmzog2rkaguko9ri669taq\", \"message\": \"- $body\n- $example\", \"title\": \"$header\"}"
