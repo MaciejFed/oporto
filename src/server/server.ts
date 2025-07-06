8d7099c8bf4a5b34589042212f2bf9a0f858766b
MaciejFedorowiat <maciejfedorowiat@gmail.com>
2025-07-06 07:58:11 +0200

    terminal

--- src/server/server.ts
+++ src/server/server.ts
@@ -259,7 +259,7 @@ app.get('/:language/in-progress', async (req: Request, res: Response) => {
 
     res.send({
       header: exercise.getDescription(),
-      bodyPrefix: exercise.getBodyPrefix().replace('Portuguese: ', ''),
+      bodyPrefix: exercise.getBodyPrefix().replace('Portuguese: ', '').replace('English: ', ''),
       body: exercise.getCorrectAnswer(),
       example: exampleSelected?.targetLanguage
     });
