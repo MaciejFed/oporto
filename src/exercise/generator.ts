87a579fd223015e84eb4b2eb3d0d7795420f17e4
MaciejFedorowiat <maciejfedorowiat@gmail.com>
2025-07-22 19:40:46 +0200

    terminal

--- src/exercise/generator.ts
+++ src/exercise/generator.ts
@@ -39,7 +39,7 @@ import { VerbOtherFormTranslationExercise } from './translation/verb-other-form-
 import { frequencyMap } from '../frequency';
 
 type ExerciseGenerator = () => Exercise[];
-export const LIMIT_FREQ = 1000;
+export const LIMIT_FREQ = 2000;
 
 export const VerbExerciseGenerator: ExerciseGenerator = () => {
   const allowedVerbTimes: VerbTimeType[] = ['presentSimple', 'pastPerfect', 'imperfect'];
